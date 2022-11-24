0834d958b78e4f43e082b5e26b266433e72d8755
Ekspresso <cost.cu4erov@mail.ru>
2022-11-24 19:36:46 +0300

    add task 16 and 17

---
+++ task17/task17.go
@@ -0,0 +1,63 @@
+//Программа принимает из 1 аргумента командной строки
+//целое число, которое необходимо найти и выводит его номер
+//в заданном массиве
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var s [10]int
+	for i := 0; i < len(s); i++ {
+		s[i] = i + 1
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Error: more than 1 search element")
+	} else if len(os.Args) < 2 {
+		fmt.Println("Error: the search element is not specified")
+	} else {
+		el, err := strconv.Atoi(os.Args[1])
+		if err == nil {
+			c := bin_search(el, s[:])
+			if c == -1 {
+				fmt.Println("Element not found or incorrect array")
+			} else {
+				fmt.Println(c)
+			}
+		}
+	}
+}
+
+//Функция бинарного поиска получает элемент, который надо найти,
+//срез массива для поиска в нём и возвращает индекс найденного элемента
+func bin_search(el int, arr []int) int {
+	l := 0
+	r := len(arr)
+	if arr[l] < arr[r-1] {
+		for i := 0; i <= len(arr)/2; i++ {
+			c := l + (r-l)/2
+			if el < arr[c] {
+				r = c
+			} else if el > arr[c] {
+				l = c
+			} else {
+				return (c)
+			}
+		}
+	} else {
+		for i := 0; i <= len(arr)/2; i++ {
+			c := l + (r-l)/2
+			if el < arr[c] {
+				l = c
+			} else if el > arr[c] {
+				r = c
+			} else {
+				return (c)
+			}
+		}
+	}
+	return (-1)
+}
