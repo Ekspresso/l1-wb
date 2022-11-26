e648295d117a317ff408b1cc761546c826ba7c16
Ekspresso <cost.cu4erov@mail.ru>
2022-11-27 00:29:38 +0300

    add task 23

---
+++ task23/task23.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	sl := []int{1, 2, 3, 4, 5, 6, 7}
+	sl = deleteElSlise(sl, 2)
+	fmt.Println(sl)
+}
+
+// Функция добавляет к срезу от начала до k-ого элемента (не включительно) срез от следующего после k элемента до конца.
+func deleteElSlise(sl []int, k int) []int {
+	sl[k] = 0
+	return append(sl[:k], sl[(k+1):]...)
+}
