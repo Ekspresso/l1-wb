84150febdaff694464f5495db78c5c0f06378431
Ekspresso <cost.cu4erov@mail.ru>
2022-11-27 01:22:44 +0300

    add task 24

---
+++ task24/task24.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{x, y}
+}
+
+func Distance(a, b *Point) float64 {
+	return math.Sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y))
+}
+func main() {
+	a := NewPoint(2.1, -2.5)
+	b := NewPoint(3.9, -3.6)
+	fmt.Println(Distance(a, b))
+}
